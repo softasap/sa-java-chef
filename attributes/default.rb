1f5899edfc54c413b36dd1d74c70db8c0b39a551
Vyacheslav Voronenko <git@voronenko.info>
2016-11-26 23:00:20 +0200

    chore(syntax) minor ruby syntax corrections

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@
 
 # Set a default name
 default['sa-java']['name'] = 'sa-java'
-default['sa-java']['java_version'] = '7'  # 6,7,8
+default['sa-java']['java_version'] = '7' # 6,7,8
 
 # For further information, see the Chef documentation (https://docs.chef.io/essentials_cookbook_attribute_files.html).
